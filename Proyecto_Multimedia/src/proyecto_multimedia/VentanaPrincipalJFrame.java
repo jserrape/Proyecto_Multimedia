8453f40f2bcd549763ff8775fef08aa70c290180
Markax <marka.phirae@gmail.com>
2017-05-08 11:36:51 +0200

    -Funcionamiento correcto de cerrar

--- Proyecto_Multimedia/src/proyecto_multimedia/VentanaPrincipalJFrame.java
+++ Proyecto_Multimedia/src/proyecto_multimedia/VentanaPrincipalJFrame.java
@@ -199,6 +199,7 @@ public class VentanaPrincipalJFrame extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         this.dispose();
+        System.exit(0);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
