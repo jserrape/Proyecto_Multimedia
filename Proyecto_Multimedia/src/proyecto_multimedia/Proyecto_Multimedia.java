a8d775b6cbaed1d0860a8b70597c297e9dfc3da7
Markax <marka.phirae@gmail.com>
2017-05-18 12:19:12 +0200

    -minor bug fixes

--- Proyecto_Multimedia/src/proyecto_multimedia/Proyecto_Multimedia.java
+++ Proyecto_Multimedia/src/proyecto_multimedia/Proyecto_Multimedia.java
@@ -5,13 +5,15 @@
  */
 package proyecto_multimedia;
 
+import java.io.IOException;
+
 
 public class Proyecto_Multimedia {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         VentanaPrincipalJFrame ventana=new VentanaPrincipalJFrame();
         ventana.setVisible(true);
